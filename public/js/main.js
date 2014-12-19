7d0e0bad0bcd610c13511d23d735849f109d3116
Carl von Buelow <cvbuelow@gmail.com>
2014-12-19 13:13:13 -0500

    Comment

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ require(['load', 'deps', 'one', 'jquery', 'pubsub'], function(load, deps, one) {
   load(deps);
 
   $.subscribe('course-loaded', function(event, course) {
-    course.hey(one);
+    course.hey(one);  // do something using common interface
   });
 
 });
